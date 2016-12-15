591abea2c010ae54f60e1cf3729be7246411d477
LiuNing <ln152315@163.com>
2016-12-15 20:47:04 +0800

    problem 263

--- leetcode/src/com/leetcode/main/Leetcode.java
+++ leetcode/src/com/leetcode/main/Leetcode.java
@@ -21,17 +21,7 @@ public class Leetcode {
 	
 	
 	
-	public boolean isUgly(int num) {
-    if (num == 1) return true;
-    if (num < 1) return false;
-    while(num != 1 ){
-    	while(num%2==0) num = num/2;
-    	while(num%3==0) num = num/3;
-    	while(num%5==0) num = num/5;
-    	if(num != 1) return false;
-    }
-    return true;
-  }
+	
 	
 	public boolean isHappy(int n) {
 		if(n < 1) return false;
