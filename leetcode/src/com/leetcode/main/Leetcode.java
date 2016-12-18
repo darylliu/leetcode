8e562bbbc5be20f49618cce6fe885f99a673cd4b
LiuNing <ln152315@163.com>
2016-12-18 20:46:43 +0800

    problem 389

--- leetcode/src/com/leetcode/main/Leetcode.java
+++ leetcode/src/com/leetcode/main/Leetcode.java
@@ -56,16 +56,7 @@ public class Leetcode {
     return result;
   }
 	
-	public char findTheDifference(String s, String t) {
-    char[] s1 = s.toCharArray();
-    char[] t1 = t.toCharArray();
-    Arrays.sort(s1);
-    Arrays.sort(t1);
-    for(int i=0;i<s.length(); i++){
-    	if(s1[i]!=t1[i]) return t1[i];
-    }
-    return t1[t1.length-1];
-  }
+	
 	
 	public int maxProfit(int[] prices) {
     int max = 0;
