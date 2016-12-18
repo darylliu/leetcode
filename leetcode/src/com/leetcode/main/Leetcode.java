0f98151068bf19fede1a5874e5ae627dc6bdfea6
LiuNing <ln152315@163.com>
2016-12-18 20:43:07 +0800

    problem 168

--- leetcode/src/com/leetcode/main/Leetcode.java
+++ leetcode/src/com/leetcode/main/Leetcode.java
@@ -41,24 +41,7 @@ public class Leetcode {
     }
   }
 	
-	public String convertToTitle(int n) {
-		HashMap<Integer,Character> map = new HashMap<Integer,Character>();
-    for(int i=0; i<26; i++){
-    	map.put(i+1,(char) ('A'+i));
-    }
-    String result = "";
-    while(n>0){
-    	int tmp = n%26;
-    	if(tmp==0){
-    		result = "Z" + result;
-    		n--;
-    	}
-    	else result = map.get(tmp) + result;
-    	n = n/26;
-    }
-    
-    return result;
-  }
+	
 	
 	public int titleToNumber(String s) {
     HashMap<Character,Integer> map = new HashMap<Character,Integer>();
