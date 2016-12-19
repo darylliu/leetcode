d621c6016868576d9e7bccca5b3f00414243f921
LiuNing <ln152315@163.com>
2016-12-19 09:27:38 +0800

    problem 171

--- leetcode/src/com/leetcode/main/Leetcode.java
+++ leetcode/src/com/leetcode/main/Leetcode.java
@@ -43,18 +43,7 @@ public class Leetcode {
 	
 	
 	
-	public int titleToNumber(String s) {
-    HashMap<Character,Integer> map = new HashMap<Character,Integer>();
-    for(int i=0; i<26; i++){
-    	map.put((char) ('A'+i), i+1);
-    }
-    int result = 0;
-    for(int i=s.length()-1; i>=0; i--){
-    	int tmp = map.get(s.charAt(i));
-    	result = (int) (result + tmp* Math.pow(26, s.length()-1-i));
-    }
-    return result;
-  }
+	
 	
 	
 	
