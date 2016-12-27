2d2b6e3db6879d18489e6ea0cdae11d7602c94bd
LiuNing <ln152315@163.com>
2016-12-27 10:37:52 +0800

    problem 404 441

---
+++ leetcode/src/com/leetcode/problem/Problem404.java
@@ -0,0 +1,29 @@
+package com.leetcode.problem;
+
+import com.leetcode.util.TreeNode;
+
+/**
+ * Sum of Left Leaves
+ * @author ln
+ *
+ */
+public class Problem404 {
+	public int sumOfLeftLeaves(TreeNode root) {
+    return valueOfLeft(root);
+	}
+	
+	public int valueOfLeft(TreeNode root){
+    if (root == null) return 0;
+    else if(root.left == null && root.right==null) return 0;
+    if(root.left == null && root.right != null) return valueOfLeft(root.right);
+    else if(root.left!=null && root.right == null){
+        if(root.left.left !=null || root.left.right !=null) return valueOfLeft(root.left);
+        else return root.left.val;
+    } 
+    else {
+        if(root.left.left !=null || root.left.right !=null) return valueOfLeft(root.left)+valueOfLeft(root.right);
+        else return root.left.val+valueOfLeft(root.right);
+    }
+   
+}
+}
