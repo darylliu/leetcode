21d35bb6a590e78305281565f699a990ea4c65f8
LiuNing <ln152315@163.com>
2017-03-07 15:54:53 +0800

    problem148 173

---
+++ leetcode/src/com/leetcode/problem/two/Problem173.java
@@ -0,0 +1,37 @@
+package com.leetcode.problem.two;
+
+import java.util.Stack;
+
+import com.leetcode.util.TreeNode;
+
+/**
+ * Binary Search Tree Iterator
+ * @author ln
+ *
+ */
+public class Problem173 {
+//	Stack<TreeNode> s = new Stack<TreeNode>();
+//  public BSTIterator(TreeNode root) {
+//      putAll(root);
+//  }
+//
+//  /** @return whether we have a next smallest number */
+//  public boolean hasNext() {
+//      return !s.isEmpty();
+//  }
+//
+//  /** @return the next smallest number */
+//  public int next() {
+//      TreeNode cur = s.pop();
+//      putAll(cur.right);
+//      return cur.val;
+//  }
+//  
+//  public void putAll(TreeNode node){
+//      while( node!=null) {
+//          s.push(node);
+//          node = node.left;
+//      }
+//      
+//  }
+}
